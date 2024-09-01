b81fd97e592e18b5d7e3730e26bd59642ab348da
Camilo <user@CAmilos-MacBook-Pro.local>
2024-09-01 13:02:19 -0700

    seeds data  set

---
+++ seed-data/users.js
@@ -0,0 +1,13 @@
+module.exports = [
+    {
+      id: 1,
+      name: "camilo",
+      last_name: "jorquera",
+      email: "cbj.jorquera@gmail.com",
+      password: "hola",
+      about:"hey! I am camilo! keen to ride anytime",
+      profile_image:"http://localhost:8080/images/342722579_117055371366248_574332884134821083_n.jpg",
+      riding_level:"expert"
+    },
+
+  ];
---
+++ seeds/users.posts.js
@@ -0,0 +1,9 @@
+const usersData = require('../seed-data/users');
+const postsData = require('../seed-data/posts');
+
+exports.seed = async function(knex) {
+  await knex('post').del();
+  await knex('user').del();
+  await knex('user').insert(usersData);
+  await knex('post').insert(postsData);
+};
